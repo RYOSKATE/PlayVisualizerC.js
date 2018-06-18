486913206601657d99c4d8d7c91f4315a45fbe23
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2018-06-18 10:31:53 +0900

    update webpack config for each mode setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const webpack = require('webpack');
+// optimization.minimizerを上書きするために必要なプラグイン
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
+
 const threadLoader = {
   loader: 'thread-loader',
   options: {
@@ -13,29 +17,54 @@ const babelLoader = {
   }
 };
 
-module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    path: require("path").resolve("./docs/"),
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]'
-  },
-  target: "node",
-  resolve: {
-    extensions: ['.js', '.json'],
-  },
-  module: {
-    rules: [{
-      exclude: /node_modules/,
-      use: [
-        { loader: 'cache-loader' },
-        threadLoader,
-        babelLoader
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === 'development';
+  return {
+    entry: {
+      index: './src/index.js',
+    },
+    output: {
+      path: require("path").resolve("./docs/js/visualizer/"),
+      filename: '[name].js',
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+    },
+    target: "node",
+    resolve: {
+      extensions: ['.ts', '.js', '.json'],
+    },
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'cache-loader' },
+          threadLoader,
+          babelLoader,
+          {
+            loader: 'ts-loader',
+            options: { happyPackMode: true }
+          }
+        ]
+      }
       ]
+    },
+    devtool: IS_DEVELOPMENT ? 'inline-source-map' : 'none',
+    optimization: {
+      // developmentモードでビルドした場合
+      // minimizer: [] となるため、consoleは残されたファイルが出力される
+      // puroductionモードでビルドした場合
+      // minimizer: [ new UglifyJSPlugin({... となるため、consoleは削除したファイルが出力される
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+          new UglifyJSPlugin({
+            uglifyOptions: {
+              compress: {
+                drop_console: true
+              }
+            }
+          })
+        ]
     }
-    ]
-  },
-  devtool: 'inline-source-map'
+  }
 }
